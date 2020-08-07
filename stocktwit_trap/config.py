2aaea0c37604d2e3da5fb7196ff9162f38aa0b43
Civil-JE <joshua.e.eastman@gmail.com>
2020-08-07 13:17:35 -0400

    initial_setup: Oops

--- stocktwit_trap/config.py
+++ stocktwit_trap/config.py
@@ -1,5 +1,5 @@
-CONSUMER_KEY = 'f9dafc6e89ba998c'
-CONSUMER_SECRET = '0d370b18ea18cc302246144e5a34455759b6c35f'  #  Shhhhh, it's a secret
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''  #  Shhhhh, it's a secret
 REQUEST_TOKEN_URL = 'https://api.stocktwits.com/api/2/oauth/token'
 AUTHORIZE_URL = 'https://api.stocktwits.com/api/2/oauth/token'
 
@@ -7,5 +7,5 @@ BASE_STOCKTWIT_URL = 'https://api.stocktwits.com/api/2/'
 
 SYMBOL_LIST = ['AAPL', 'GOOG', 'APLT', 'AXSM']
 
-ALPHA_VANTAGE_KEY = 'HO4AP7G3I61BBY12'
+ALPHA_VANTAGE_KEY = ''
 ALPHA_VANTAGE_BASE_URL = 'https://www.alphavantage.co/query?function='
